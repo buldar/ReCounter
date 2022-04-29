a6e7461af2148d3e678b1df82a4bc8c2caa59d95
Buldar <buldar666@gmail.com>
2022-04-29 14:57:46 +0300

    added some CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,42 @@ import React, {useState} from 'react';
 import './App.css';
 
 
-
 function App() {
 
-    let [value, setValue] = useState<number>(0)
+    const errors = {
+        wrongOptions: 'Wrong options'
+    }
+
+    let [value, setValue] = useState(0)
 
     const increaseValue = () => {
-        setValue(value+1)
+        setValue(value + 1)
     }
 
     const decreaseValue = () => {
-        setValue(value-1)
+        setValue(value - 1)
     }
 
     const restart = () => {
         setValue(0)
     }
 
-    let [options, setOptions] = useState({min:0, max:0})
+    let [options, setOptions] = useState({min: -5, max: 5})
+
+    let [menu, setMenu] = useState(true)
 
     return (
         <div className="App">
             <div className='Header'>
-                <h1>COUNTER</h1>
+                <h1>HEADER</h1>
             </div>
             <div className='Counter'>
                 <div className='Display'>
                     <p>{value}</p>
                 </div>
                 <div className='Functions'>
-                    <button onClick={decreaseValue}>-</button>
-                    <button onClick={increaseValue}>+</button>
+                    <button disabled={value === options.min} onClick={decreaseValue}>-</button>
+                    <button disabled={value === options.max} onClick={increaseValue}>+</button>
                     <button onClick={restart}>re</button>
                 </div>
             </div>
