32dfa4ad75dd1c8e20aa9674c2ee760d1da06f17
Buldar <buldar666@gmail.com>
2022-04-29 13:12:07 +0300

    first layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export let state = {
+    currentValue: 0,
+
+}
+
 function App() {
     return (
         <div className="App">
-            <h1>COUNTER</h1>
+            <div className='Header'>
+                <h1>COUNTER</h1>
+            </div>
+            <div className='Counter'>
+                <div className='Display'>
+                    <p>{state.currentValue}</p>
+                </div>
+                <div className='Functions'>
+                    <button>-</button>
+                    <button>+</button>
+                    <button>re</button>
+                </div>
+            </div>
         </div>
     );
 }
